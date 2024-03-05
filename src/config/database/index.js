bc0ec305b42a429830ce5820d590a8ed0de6cdac
ndhnws14 <donghonew14@gmail.com>
2024-03-05 21:06:57 +0700

    connection mongodb

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -5,7 +5,7 @@ async function connect() {
   try {
     await mongoose
       .connect(
-        'mongodb://localhost:27017/webchat-win', {
+        'mongodb+srv://dongho:dongho@cluster0.clh0zxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
           useNewUrlParser: true,
           useUnifiedTopology: true,
       })
