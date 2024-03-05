2544a8eff1920fa04ebe82dbaa0a60c17f7075c1
ndhnws14 <donghonew14@gmail.com>
2024-03-05 20:44:47 +0700

    connect mongooes db

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -3,13 +3,12 @@ const Schema = mongoose.Schema;
 
 async function connect() {
   try {
-    mongoose
+    await mongoose
       .connect(
-        // URL to connect .
-        //NOTE : USE atlas mongoBD
-        "",
-        // *******88888*******
-      )
+        'mongodb://localhost:27017/webchat-win', {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      })
       .then(() => console.log("connection established"));
   } catch (error) {
     console.log("connection error: " + error);
